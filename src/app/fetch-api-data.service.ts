763f770f0d7aa6bbe14d347192298d9968d4445b
cmr927 <142535038+cmr927@users.noreply.github.com>
2024-08-01 15:07:00 -0400

    Created the user registration modal

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'YOUR_HOSTED_API_URL_HERE/';
+const apiUrl = 'https://movies-myflix-cmr927-6d25967ba551.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
